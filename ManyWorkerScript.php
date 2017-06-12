b846a91a654a4147c63611d4c97a30d93651d913
wanghongjun <wanghongjun@wanghongjundeMacBook-Pro.local>
2017-06-12 13:50:28 +0800

    fix

--- ManyWorkerScript.php
+++ ManyWorkerScript.php
@@ -8,11 +8,16 @@
 
 class ManyWorkerScript {
 
-    public static $_debugLog = '/tmp/debug.log';  //调试输出的log文件
+    public static $_debugLog = '/tmp/debug.log';  // 调试输出的log文件 .
 
+    public static $_maxWorkerNum = 10;  // 某台机器所允许的最大进程数, 后期需要自动计算进程运算占用的内存自动优化 .
+
+    public static $_killChangeProcessResult = false; // 杀死某个正在运行的进程是否影响结果, 默认不影响 .
+
+    public static $_currentWorkerNum = 0; // 当前正在运行的进程数 .
     public function __construct()
     {
-        // init初始化的一些东西.
+        // init初始化的一些东西 .
         self::checkPcntl();
     }
 
@@ -22,7 +27,7 @@ class ManyWorkerScript {
         if (!function_exists('pcntl_signal_dispatch')) {
             declare(ticks = 10);
         }
-        if (!function_exists(pcntl_signal())) {
+        if (!function_exists('pcntl_signal')) {
             exit('PHP lacks PCNTL extensions');
         }
     }
@@ -65,10 +70,43 @@ class ManyWorkerScript {
         fclose(STDERR);
     }
 
+    /**
+     * @param int $workerNum
+     * 多进程主程序入口
+     */
+    public function main($workerNum = 2)
+    {
+        self::daemonize();
 
+        $workerNum = $workerNum > self::$_maxWorkerNum ? self::$_maxWorkerNum : $workerNum;
+
+        while (true) {
+
+            // 实时监测各种信号 .
+            if (function_exists('pcntl_signal_dispatch')) {
+                pcntl_signal_dispatch();
+            }
+
+            // fork出指定数量的进程  .
+            if (self::$_currentWorkerNum < $workerNum) {
+                $pid = pcntl_fork();
+                if ($pid > 0) {
+                    self::$_currentWorkerNum++;
+                } elseif ($pid == 0){
+                    //子进程直接退出了, 不要重复fork
+                    return ;
+                } else {
+                    exit('fork fatal error');
+                    break;
+                }
+            }
+
+            sleep(1);
+        }
+    }
     /**
      * @param $message
-     *
+     * return null
      */
     public static function errorLog($message, $isKill = false) {
         error_log($message . "\n", 3, self::$_debugLog);
--- cronn_test.php
+++ cronn_test.php
@@ -1,23 +1,27 @@
 <?php
-
-require 'DaemonScript.php';
-
-$daemon = new Comm_DaemonScript(__CLASS__, __CLASS__, false);
-
-$daemon->daemonize();
-
-$daemon->start(5);
-//$pid     = posix_getpid();
-//$monitor = $daemon->getMonitorFile();
-//$daemon->updateRunTime($pid, $monitor);
-
-function my_log($msg) {
-    error_log($msg . "\n", 3, '/tmp/log.log');
-}
-
-sleep(1000);
+//
+//require 'DaemonScript.php';
+//
+//$daemon = new Comm_DaemonScript(__CLASS__, __CLASS__, false);
+//
+//$daemon->daemonize();
+//
+//$daemon->start(5);
+////$pid     = posix_getpid();
+////$monitor = $daemon->getMonitorFile();
+////$daemon->updateRunTime($pid, $monitor);
+//
+//function my_log($msg) {
+//    error_log($msg . "\n", 3, '/tmp/log.log');
+//}
+//
+//sleep(1000);
 
 
 require 'ManyWorkerScript.php';
 
+$obj = new ManyWorkerScript();
+$obj->main(5);
+
+sleep(100);
 
